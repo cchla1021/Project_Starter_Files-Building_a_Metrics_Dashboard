2f44f413ea3b9897ce0c2ae10faee71a8ba5b850
cchla1021 <chih-hsien.chien@intel.com>
2022-07-16 18:40:13 -0400

    Update app.py

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, jsonify, request
+import threading
+import requests
+import random
+import time
 from prometheus_flask_exporter import PrometheusMetrics
+import logging
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app, group_by='endpoint')
 
-metrics.info('app_info', 'Application Info', version='1.0.3')
-
-# Register extra metrics
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
 metrics.register_default(
     metrics.counter(
         'by_path_counter', 'Request count by request paths',
@@ -14,18 +20,63 @@ metrics.register_default(
     )
 )
 
-# Apply the same metric to all of the endpoints
-endpoint_counter = metrics.counter(
-    'endpoint_counter', 'Request count by endpoints',
+by_endpoint_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by request endpoint',
     labels={'endpoint': lambda: request.endpoint}
 )
+endpoints = ('error', 'foo', 'healthz')
 
-# Root route
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer('frontend')
+tracing = FlaskTracing(tracer, True, app)
+
+def random_endpoint():
+    while True:
+        try:
+            target = random.choice(endpoints)
+            requests.get("http://app:8081/%s" % target, timeout=1)
+
+        except:
+            pass
+
 @app.route('/')
-@endpoint_counter
+@by_endpoint_counter
 def homepage():
     return render_template("main.html")
+    with tracer.start_span('random_endpoint') as span:
+        threading.Thread(target=random_endpoint).start()
+        for _ in range(4):
+            thread = threading.Thread(target=random_endpoint)
+            thread.daemon = True
+            thread.start()
+
+        while True:
+            time.sleep(1)
+    
 
 
-if __name__ == "__main__":
+@app.route('/healthz')
+@by_endpoint_counter
+def healthcheck():
+    app.logger.info('Status request successfull')
+    return jsonify({"result": "OK - healthy"})
+
+if __name__ == "__main__":    
     app.run()
