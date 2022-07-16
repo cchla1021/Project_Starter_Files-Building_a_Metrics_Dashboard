ae6e071f2593454b83a9d892ae21e664d729cb7d
cchla1021 <chih-hsien.chien@intel.com>
2022-07-16 17:17:00 -0400

    Update app.py

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -6,6 +6,7 @@ import time
 from prometheus_flask_exporter import PrometheusMetrics
 import logging
 from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app, group_by='endpoint')
