5e15b47e8047b5aee9bbe2a312fad4da5e8486eb
cchla1021 <chih-hsien.chien@intel.com>
2022-07-16 21:51:50 -0400

    Update app.py

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -6,6 +6,7 @@ import time
 from prometheus_flask_exporter import PrometheusMetrics
 import logging
 from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app, group_by='endpoint')
@@ -44,6 +45,7 @@ def init_tracer(service):
     return config.initialize_tracer()
 
 tracer = init_tracer('frontend')
+tracing = FlaskTracing(tracer, True, app)
 
 def random_endpoint():
     while True:
